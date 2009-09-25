e373e0c94cdbe58771d0a810ab27807b827244f0
Stefan Kudwien <s-m-k@gmx.net>
2009-09-25 03:49:16 +0200

    Added template file for nodes acting as interlinked terms.

---
+++ theme/interlink-node.tpl.php
@@ -0,0 +1,37 @@
+<?php
+// $Id: node.tpl.php,v 1.5 2007/10/11 09:51:29 goba Exp $
+?>
+<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
+
+  <?php print $picture ?>
+
+  <?php if ($page == 0): ?>
+    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  <?php endif; ?>
+
+  <?php if ($submitted): ?>
+    <span class="submitted"><?php print $submitted; ?></span>
+  <?php endif; ?>
+
+  <dl class="interlink-term">
+    <dt><dfn id="interlink-node-<?php print $node->nid; ?>"><?php print $title ?></dfn></dt>
+    <dd>
+      <div class="content clear-block">
+        <?php print $content ?>
+      </div>
+    </dd>
+  </dl>
+
+  <div class="clear-block">
+    <div class="meta">
+    <?php if ($taxonomy): ?>
+      <div class="terms"><?php print $terms ?></div>
+    <?php endif;?>
+    </div>
+
+    <?php if ($links): ?>
+      <div class="links"><?php print $links; ?></div>
+    <?php endif; ?>
+  </div>
+
+</div>
